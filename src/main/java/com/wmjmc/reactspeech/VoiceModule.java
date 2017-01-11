2089d73f8502a5c045b616780190d0982ea44e49
de-git <de-git@github.com>
2017-01-11 00:53:14 +0000

    Apply ActivityEventListener interface changes to onActivityResult method

--- src/main/java/com/wmjmc/reactspeech/VoiceModule.java
+++ src/main/java/com/wmjmc/reactspeech/VoiceModule.java
@@ -70,6 +70,15 @@ public class VoiceModule extends ReactContextBaseJavaModule implements ActivityE
     }
 
     @Override
+    public void onActivityResult(
+        Activity activity,
+        int requestCode,
+        int resultCode,
+        Intent data
+    ) {
+        this.onActivityResult(requestCode, resultCode, data);
+    }
+
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (mVoicepromise == null) {
             return;
@@ -102,6 +111,10 @@ public class VoiceModule extends ReactContextBaseJavaModule implements ActivityE
                 mVoicepromise = null;
                 break;
         }
+    }
+
+    public void onNewIntent(Intent intent) {
+        // no-op
     }
 
     private String getPrompt(String prompt){
